7fd996daa39397e9744ff0226f741efa8dab9797
woxinxie1234 <woxinxie1234@163.com>
2021-09-16 08:31:53 +0800

    a simple example for autograd

--- code/20_loss/autograd_basic_1_learningSimple.py
+++ code/20_loss/autograd_basic_1_learningSimple.py
@@ -1,3 +1,6 @@
+"""
+对照图片查看，效果更佳
+"""
 import torch
 
 
@@ -10,7 +13,7 @@ def autograd(grad_fn, gradient):
         gradients = item[0](item[1])
         functions = [x[0] for x in item[0].next_functions]
         if type(gradients) is not tuple:
-            gradients = (gradients, )
+            gradients = (gradients,)
         for grad, func in zip(gradients, functions):
             if type(func).__name__ == 'AccumulateGrad':
                 if hasattr(func.variable, 'auto_grad'):
@@ -20,6 +23,7 @@ def autograd(grad_fn, gradient):
             else:
                 queue.append([func, grad])
 
+
 A = torch.tensor([3.], requires_grad=True)
 B = torch.tensor([2.], requires_grad=True)
 C = A ** 2
@@ -28,4 +32,4 @@ E = C * D
 F = D + E
 
 autograd(F.grad_fn, torch.tensor(1))
-print(A.auto_grad, B.auto_grad)         # tensor(24., grad_fn=<UnbindBackward>) tensor(40., grad_fn=<AddBackward0>)
+print(A.auto_grad, B.auto_grad)  # tensor(24., grad_fn=<UnbindBackward>) tensor(40., grad_fn=<AddBackward0>)
