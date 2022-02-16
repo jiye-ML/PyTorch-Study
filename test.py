b1b6d136fcb07689303ab0fa5e3f7a9ad692328e
woxinxie1234 <woxinxie1234@163.com>
2022-02-16 21:32:08 +0800

    torch 1.8版本模型和1.4模型兼容问题

--- test.py
+++ test.py
@@ -1,31 +1,27 @@
 import torch
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore') #ignore warnings
 
+x = torch.linspace(-np.pi, np.pi, 2000)
+y = torch.sin(x)
 
-# 这一例子仅可用于每个op只产生一个输出的情况，且效率很低（由于对于某一节点，每次未等待所有梯度反传至此节点，就直接将本次反传回的梯度直接反传至叶节点）
-def autograd(grad_fn, gradient):
-    auto_grad = {}
-    queue = [[grad_fn, gradient]]
-    while queue != []:
-        item = queue.pop()
-        gradients = item[0](item[1])
-        functions = [x[0] for x in item[0].next_functions]
-        if type(gradients) is not tuple:
-            gradients = (gradients, )
-        for grad, func in zip(gradients, functions):
-            if type(func).__name__ == 'AccumulateGrad':
-                if hasattr(func.variable, 'auto_grad'):
-                    func.variable.auto_grad = func.variable.auto_grad + grad
-                else:
-                    func.variable.auto_grad = grad
-            else:
-                queue.append([func, grad])
+p = torch.tensor([1, 2, 3])
+xx = x.unsqueeze(-1).pow(p)
 
-A = torch.tensor([3.], requires_grad=True)
-B = torch.tensor([2.], requires_grad=True)
-C = A ** 2
-D = B ** 2
-E = C * D
-F = D + E
+model = torch.nn.Sequential(
+    torch.nn.Linear(3, 1),
+    torch.nn.Flatten(0, 1)
+)
+loss_fn = torch.nn.MSELoss(reduction='sum')
 
-autograd(F.grad_fn, torch.tensor(1))
-print(A.auto_grad, B.auto_grad)         # tensor(24., grad_fn=<UnbindBackward>) tensor(40., grad_fn=<AddBackward0>)
+learning_rate = 1e-3
+optimizer = torch.optim.RMSprop(model.parameters(), lr=learning_rate)
+for t in range(1, 1001):
+    y_pred = model(xx)
+    loss = loss_fn(y_pred, y)
+    if t % 100 == 0:
+        print('No.{: 5d}, loss: {:.6f}'.format(t, loss.item()))
+    optimizer.zero_grad() # 梯度清零
+    loss.backward() # 反向传播计算梯度
+    optimizer.step() # 梯度下降法更新参数
